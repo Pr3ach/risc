8d66b3d49e0d8647a3515400ea3b136d364999bd
Pr3ach <Pr3acher777h@gmail.com>
2014-10-05 19:08:29 +0200

    testing process_irc()

--- src/risc/risc.py
+++ src/risc/risc.py
@@ -1788,8 +1788,17 @@ class Risc():
             return None
         msg = raw_msg[0].split(':')[2]
         nick = raw_msg[0].split('!')[0][1:]
+        url_re = re.compile(r'(?:http|ftp)s?://(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)'
+                r'+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d'
+                r'{1,3}|\[?[A-F0-9]*:[A-F0-9:]+\]?)(?::\d+)?(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-        print nick+' '+msg
+        is_url = re.match(url_re, msg)
+
+        if is_url:
+            url_dump = urllib.urlopen(is_url[0]).read()
+            print "dump: "
+            print url_dump
+
         return None
 
     def _on_privmsg(self, msg):
