0c32c9ca6a6b5e69d0b02de0ba83113ad84d57e2
Pr3ach <Pr3acher777h@gmail.com>
2014-10-13 15:06:20 +0200

    process_irc() - testing (7)

--- src/risc/risc.py
+++ src/risc/risc.py
@@ -1961,6 +1961,7 @@ class Risc():
         nick = raw_msg[0].split('!')[0][1:]
         url_re = re.compile(r'(?:http|ftp)s?://(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\[?[A-F0-9]*:[A-F0-9:]+\]?)(?::\d+)?(?:/?|[/?]\S+)$', re.IGNORECASE)
 
+        print "msg = " + msg
         print str(re.match(url_re, msg))
 
         return None
