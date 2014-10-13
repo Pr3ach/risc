c65c1cc8c7543cf313e89d5875a8b2d7df90b442
Pr3ach <Pr3acher777h@gmail.com>
2014-10-13 15:17:23 +0200

    process_irc() - testing (9)

--- src/risc/risc.py
+++ src/risc/risc.py
@@ -1959,10 +1959,12 @@ class Risc():
             return None
         msg = ':'.join(raw_msg[0].split(':')[2:])
         nick = raw_msg[0].split('!')[0][1:]
-        url_re = re.compile(r'(?:http|ftp)s?://(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\[?[A-F0-9]*:[A-F0-9:]+\]?)(?::\d+)?(?:/?|[/?]\S+)$', re.IGNORECASE)
+        re_url = re.compile(r'(?:http|ftp)s?://(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\[?[A-F0-9]*:[A-F0-9:]+\]?)(?::\d+)?(?:/?|[/?]\S+)$', re.IGNORECASE)
 
-        print "msg = " + msg
-        print str(re.match(url_re, msg))
+        check_url = re.match(re_url, msg)
+        if check_url:
+            url = urllib.urlopen(check_url.group(0))
+            print url.read()
 
         return None
 
