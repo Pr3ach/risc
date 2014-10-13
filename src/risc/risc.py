b73c9315cfc61d046daa12091641b2c656e4d566
Pr3ach <Pr3acher777h@gmail.com>
2014-10-13 14:57:39 +0200

    process_irc() - testing (5)

--- src/risc/risc.py
+++ src/risc/risc.py
@@ -1955,6 +1955,8 @@ class Risc():
         return ''.join(l)
 
     def process_irc(self, raw_msg):
+        if not re.search(' PRIVMSG ', raw_msg):
+            return None
         msg = raw_msg[0].split(':')[2]
         nick = raw_msg[0].split('!')[0][1:]
         url_re = re.compile(r'(?:http|ftp)s?://(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\[?[A-F0-9]*:[A-F0-9:]+\]?)(?::\d+)?(?:/?|[/?]\S+)$', re.IGNORECASE)
