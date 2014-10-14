01cf0f81d404c8dec13ae680b0d20915ae38a031
Pr3ach <Pr3acher777h@gmail.com>
2014-10-14 12:30:11 +0200

    debug

--- src/risc/risc.py
+++ src/risc/risc.py
@@ -1348,6 +1348,7 @@ class Risc():
         try:
             sv = Sv(ip, port, '', self.debug)
         except Exception, e:
+            print type(e)
             return COLOR['boldred']+"Error: Exception raised: Couldn't get server status from "+ip+":"+port+": "+str(e)+COLOR['rewind']
             
         if sv.clientsList == -1:
