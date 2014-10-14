86c7c34084dad5f76a202887b53fd84735048b3c
Pr3ach <Pr3acher777h@gmail.com>
2014-10-14 10:01:05 +0200

    process_irc() - imporved regex

--- src/risc/risc.py
+++ src/risc/risc.py
@@ -1961,13 +1961,14 @@ class Risc():
             return None
         msg = ':'.join(raw_msg[0].split(':')[2:])
         nick = raw_msg[0].split('!')[0][1:]
-        re_url = re.compile(r'(?:http|ftp)s?://(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\[?[A-F0-9]*:[A-F0-9:]+\]?)(?::\d+)?(?:/?|[/?]\S+)$', re.IGNORECASE)
+re_url = re.compile(r'(?:http|ftp)s?://(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\[?[A-F0-9]*:[A-F0-9:]+\]?)(?::\d+)?(?:/?|[/?]\S+)\s*$', re.IGNORECASE)
 
         check_url = re.match(re_url, msg)
 
         if check_url:
             try:
-                self.privmsg(self.channel, str(lxml.html.parse(urllib.urlopen(check_url.group(0))).find(".//title").text)+" (at "+str(tld.get_tld(check_url.group(0))+')'))
+                self.privmsg(self.channel, str(lxml.html.parse(urllib.urlopen(check_url.group(0))).find(".//title").text)+\
+                            " (at "+str(tld.get_tld(check_url.group(0))+')'))
             except Exception, e:
                 self.debug.error('process_irc: Exception: %s - Ret' %e)
                 return None
