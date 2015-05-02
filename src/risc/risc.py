c8ac2f091614e233ac1b2891098eca199b8c533a
Pr3acher <Pr3acher777h@gmail.com>
2015-05-02 10:46:14 +0200

    fixing title bug

--- src/risc/risc.py
+++ src/risc/risc.py
@@ -2596,10 +2596,9 @@ class Risc():
 
                 if title is None:
                     continue
-                self.debug.debug(str(type(title)))
 
-                title = lxml.html.fromstring(title.text).find(".//title").text.encode("ascii", errors="backslashreplace")+" (at "+str(tld.get_tld(url))+')'
                 self.debug.debug(str(title.text))
+                title = lxml.html.fromstring(title.text).find(".//title").text.encode("ascii", errors="backslashreplace")+" (at "+str(tld.get_tld(url))+')'
                 self.privmsg(self.channel, "Title: " + title)
             except Exception, e:
                 self.debug.error('process_irc: Exception: %s - Ret' % e)
